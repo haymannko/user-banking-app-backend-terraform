1a60f9f65334172551b0c1910096e6dbbf10ea67
Mintawlon <minhtawlon111@gmail.com>
2025-10-19 15:06:05 +0700

    IS/TIC-003

---
+++ app/scanPayment-service/src/main/java/com/corporatebanking/scanpaymentservice/ScanPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.corporatebanking.scanpaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScanPaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScanPaymentServiceApplication.class, args);
+    }
+
+}

e282268bf9b04f725d784b0ebeaab2ec56499865
Mintawlon <minhtawlon111@gmail.com>
2025-10-20 17:27:16 +0700

    IS/TIC_010

--- app/gateway-service/src/main/java/com/personalbanking/gateway/dto/nickname/CreateNicknameRequestDto.java
+++ app/gateway-service/src/main/java/com/personalbanking/gateway/dto/nickname/CreateNicknameRequestDto.java
@@ -1,5 +1,6 @@
 package com.personalbanking.gateway.dto.nickname;
 
+// new service creation for the project
 public record CreateNicknameRequestDto(
 		Long fromAccount,Long toAccount,String nickname,Long createdBy
 ) {}

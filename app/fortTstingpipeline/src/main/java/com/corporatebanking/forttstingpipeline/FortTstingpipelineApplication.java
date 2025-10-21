45d416815d59eb234b63405b1ec2848caf30e26a
Mintawlon <minhtawlon111@gmail.com>
2025-10-21 16:48:47 +0700

    addnewServiceforTesting

---
+++ app/fortTstingpipeline/src/main/java/com/corporatebanking/forttstingpipeline/FortTstingpipelineApplication.java
@@ -0,0 +1,13 @@
+package com.corporatebanking.forttstingpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortTstingpipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FortTstingpipelineApplication.class, args);
+    }
+
+}
---
+++ app/fortTstingpipeline/src/test/java/com/corporatebanking/forttstingpipeline/FortTstingpipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corporatebanking.forttstingpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortTstingpipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
